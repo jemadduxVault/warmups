2250858d51d29480b4797cdfa5402e0bb68534cf
Jeff Casimir <jeff@casimircreative.com>
2013-02-15 09:08:02 -0700

    Solutions

---
+++ 04-bob/bob.rb
@@ -0,0 +1,36 @@
+class Bob
+  def chat(input)
+    puts response_for(input)
+  end
+
+  def response_for(input)
+    if shouting?(input)
+      'Woah, chill out!'
+    elsif question?(input)
+      'Sure.'
+    elsif statement?(input)
+      'Whatever.'
+    else
+      'Fine, be that way.'
+    end
+  end
+
+  def statement?(input)
+    input.length > 0
+  end
+
+  def question?(input)
+    input.end_with?("?")
+  end
+
+  def shouting?(input)
+    !input.empty? && input == input.upcase
+  end
+end
+
+puts "Hey bro, wassup?"
+bob = Bob.new
+while true
+  input = gets.chomp.strip
+  bob.chat(input)
+end
