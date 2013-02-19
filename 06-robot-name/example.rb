32d344fd0fe4273a266a7ff8a1df885d1c59ad36
Katrina Owen <katrina.owen@gmail.com>
2013-02-19 09:15:12 -0700

    Add example code for robot name

---
+++ 06-robot-name/example.rb
@@ -0,0 +1,48 @@
+class Robot
+  def name
+    @name ||= "#{prefix}#{suffix}"
+  end
+
+  def reset
+    @name = nil
+  end
+
+  private
+
+  def prefix
+    alphabet.shuffle[0..1].join('')
+  end
+
+  def suffix
+    rand(899) + 100
+  end
+
+  def alphabet
+    %w(A B C D E F G H I J K L M N O P Q R S T U V W X Y Z)
+  end
+end
+
+require 'minitest/autorun'
+class RobotTest < MiniTest::Unit::TestCase
+  def test_has_name
+    assert_match /\w{2}\d{3}/, Robot.new.name
+  end
+
+  def test_name_sticks
+    robot = Robot.new
+    robot.name
+    assert_equal robot.name, robot.name
+  end
+
+  def test_different_robots_have_different_names
+    assert Robot.new.name != Robot.new.name
+  end
+
+  def test_reset_name
+    robot = Robot.new
+    name = robot.name
+    robot.reset
+    name2 = robot.name
+    assert name != name2
+  end
+end
