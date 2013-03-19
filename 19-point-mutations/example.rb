bc860663d0cd3d312d981dcc2ff7b091bcdac476
Katrina Owen <katrina.owen@gmail.com>
2013-03-19 14:55:07 -0600

    Add example for point mutation warmup

---
+++ 19-point-mutations/example.rb
@@ -0,0 +1,14 @@
+class DNA
+
+  attr_reader :strand
+  def initialize(strand)
+    @strand = strand
+  end
+
+  def hamming_distance(other_strand)
+    strand.chars.zip(other_strand.chars).reject do |a, b|
+      a == b || b.nil?
+    end.size
+  end
+
+end
