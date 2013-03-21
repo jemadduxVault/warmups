a0b7595348b73798a714747d9598996692ce1e72
Katrina Owen <katrina.owen@gmail.com>
2013-03-21 08:40:07 -0600

    Update series product warmup

--- 21-largest-series-product/README.md
+++ 21-largest-series-product/README.md
@@ -1,8 +1,11 @@
 # Largest product in a series
 
-Write a program that, when given a string of digits, can calculate the largest product for a series of consecutive digits of length n.
+Write a program that, when given a string of digits, can calculate the largest
+product for a series of consecutive digits of length n.
 
-For example, for the input `'0123456789'`, the largest product for a series of 3 digits is 504 (7 * 8 * 9), and the largest product for a series of 5 digits is 15120 (5 * 6 * 7 * 8 * 9).
+For example, for the input `'0123456789'`, the largest product for a series of
+3 digits is 504 (7 * 8 * 9), and the largest product for a series of 5 digits
+is 15120 (5 * 6 * 7 * 8 * 9).
 
 For the input `'73167176531330624919225119674426574742355349194934'`, the
 largest product is 23520.
--- 21-largest-series-product/test.rb
+++ 21-largest-series-product/test.rb
@@ -7,10 +7,30 @@ class SeriesTest < MiniTest::Unit::TestCase
     assert_equal (0..9).to_a, Series.new("0123456789").digits
   end
 
+  def test_slices_of_2
+    skip
+    series = Series.new("01234")
+    expected = [[0, 1], [1, 2], [2, 3], [3, 4]]
+    assert_equal expected, series.slices(2)
+  end
+
+  def test_slices_of_3
+    skip
+    series = Series.new("01234")
+    expected = [[0, 1, 2], [1, 2, 3], [2, 3, 4]]
+    assert_equal expected, series.slices(3)
+  end
+
   def test_largest_product_of_2
     skip
     series = Series.new("0123456789")
     assert_equal 72, series.largest_product(2)
+  end
+
+  def test_largest_product_of_a_tiny_number
+    skip
+    series = Series.new("12")
+    assert_equal 2, series.largest_product(2)
   end
 
   def test_largest_product_of_2_shuffled
@@ -42,6 +62,14 @@ class SeriesTest < MiniTest::Unit::TestCase
     s = '73167176531330624919225119674426574742355349194934'
     series = Series.new(s)
     assert_equal 23520, series.largest_product(6)
+  end
+
+  def test_slices_bigger_than_number
+    skip
+    series = Series.new("123")
+    assert_raises ArgumentError do
+      series.largest_product(4)
+    end
   end
 end
 
