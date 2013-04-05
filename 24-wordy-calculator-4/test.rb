06e6a0e1853a2a732ec1cfcc3d0c1e190680ad19
Katrina Owen <katrina.owen@gmail.com>
2013-04-05 10:29:57 -0600

    Improve tests so Aimee can't cheat.

--- 24-wordy-calculator-4/test.rb
+++ 24-wordy-calculator-4/test.rb
@@ -9,7 +9,7 @@ class WordProblemTest < MiniTest::Unit::TestCase
 
   def test_add_2
     skip
-    assert_equal 3, WordProblem.new('What is 1 plus 2?').answer
+    assert_equal 55, WordProblem.new('What is 53 plus 2?').answer
   end
 
   def test_add_more_digits
@@ -68,4 +68,12 @@ class WordProblemTest < MiniTest::Unit::TestCase
       WordProblem.new('What is 53 cubed?').answer
     end
   end
+
+  def test_irrelevant
+    skip
+    assert_raises ArgumentError do
+      WordProblem.new('Who is the president of the United States?').answer
+    end
+  end
+
 end
