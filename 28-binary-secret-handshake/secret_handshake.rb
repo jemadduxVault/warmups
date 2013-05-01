4d8db65ebed300a7b745d8c1099654ef783abb1f
John Maddux <jemaddux@gmail.com>
2013-04-30 18:05:15 -0600

    Worked with Josh

--- 28-binary-secret-handshake/secret_handshake.rb
+++ 28-binary-secret-handshake/secret_handshake.rb
@@ -2,17 +2,30 @@ class SecretHandshake
   attr_reader :binary
 
   def initialize(binary)
-    @binary = binary.rjust(5,"0").split("").reverse
+    @binary = binary.to_i(2)
+  end
+
+  def  possible_commands
+    @possible_commands ||= {0b1 => "wink", 0b10 => "double blink", 0b100 => "close your eyes", 0b1000 => "jump", 0b10000 => lambda { |says| says.reverse!}}
   end
 
   def commands
-    says = []
-    says << "wink" if binary[0] == "1"
-    says << "double blink" if binary[1] == "1"
-    says << "close your eyes" if binary[2] == "1"
-    says << "jump" if binary[3] == "1"
-    says = says.reverse if binary[4] == "1"
-    says = [""] if says == []
-    says
+    # says = [] # Array.new
+    possible_commands.each_with_object([]) do |(little_binary, instruction), says|
+      next unless (binary & little_binary) > 0
+      proc = instruction if instruction.respond_to?(:call)
+      proc ||= lambda { |says| says << instruction }
+      proc.call(says)
+    end
+
+
+
+    # says << "wink" if binary[0] == "1"
+    # says << "double blink" if binary[1] == "1"
+    # says << "close your eyes" if binary[2] == "1"
+    # says << "jump" if binary[3] == "1"
+    # says = says.reverse if binary >= 0b10000
+    # says = [""] if says == []
+    # says
   end
 end
