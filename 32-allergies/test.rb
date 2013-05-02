228521e6c0c7caa158176163ffb0870b0dde0ead
Chuck Lauer Vose <vosechu@gmail.com>
2013-05-01 17:20:36 -0700

    add skips back into tests

--- 32-allergies/test.rb
+++ 32-allergies/test.rb
@@ -10,41 +10,49 @@ class AllergiesTest < MiniTest::Unit::TestCase
   end
 
   def test_allergic_to_just_eggs
+    skip
     allergies = Allergies.new(1)
     assert_equal ['eggs'], allergies.list
   end
 
   def test_allergic_to_just_peanuts
+    skip
     allergies = Allergies.new(2)
     assert_equal ['peanuts'], allergies.list
   end
 
   def test_allergic_to_just_strawberries
+    skip
     allergies = Allergies.new(8)
     assert_equal ['strawberries'], allergies.list
   end
 
   def test_allergic_to_eggs_and_peanuts
+    skip
     allergies = Allergies.new(3)
     assert_equal ['eggs', 'peanuts'], allergies.list
   end
 
   def test_allergic_to_more_than_eggs_but_not_peanuts
+    skip
     allergies = Allergies.new(5)
     assert_equal ['eggs', 'shellfish'], allergies.list
   end
 
   def test_allergic_to_lots_of_stuff
+    skip
     allergies = Allergies.new(248)
     assert_equal ["strawberries", "tomatoes", "chocolate", "pollen", "cats"], allergies.list
   end
 
   def test_allergic_to_everything
+    skip
     allergies = Allergies.new(255)
     assert_equal ["eggs", "peanuts", "shellfish", "strawberries", "tomatoes", "chocolate", "pollen", "cats"], allergies.list
   end
 
   def test_no_allergies_means_not_allergic
+    skip
     allergies = Allergies.new(0)
     assert !allergies.allergic_to?('peanuts')
     assert !allergies.allergic_to?('cats')
@@ -52,16 +60,19 @@ class AllergiesTest < MiniTest::Unit::TestCase
   end
 
   def test_is_allergic_to_eggs
+    skip
     allergies = Allergies.new(1)
     assert allergies.allergic_to?('eggs')
   end
 
   def test_allergic_to_eggs_in_addition_to_other_stuff
+    skip
     allergies = Allergies.new(5)
     assert allergies.allergic_to?('eggs')
   end
 
   def test_ignore_non_allergen_score_parts
+    skip
     allergies = Allergies.new(509)
     assert_equal ["eggs", "shellfish", "strawberries", "tomatoes", "chocolate", "pollen", "cats"], allergies.list
   end
