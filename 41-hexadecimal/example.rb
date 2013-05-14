19f00fd8acad3fd62e06aba0df6f9b2ebca82ca1
John Maddux <jemaddux@gmail.com>
2013-05-14 09:54:03 -0600

    Another solution

--- 41-hexadecimal/example.rb
+++ 41-hexadecimal/example.rb
@@ -1,23 +1,22 @@
 class Hexadecimal
   attr_reader :hex
 
-  def initialize(input)
-    @hex = input
+  def initialize(hex)
+    @hex = hex
   end
 
   def to_decimal
     sum = 0
     hex.split("").reverse.each_with_index do |h,i|
       num = h.to_i
-      num = 10 if h.downcase == "a"
-      num = 11 if h.downcase == "b"
-      num = 12 if h.downcase == "c"
-      num = 13 if h.downcase == "d"
-      num = 14 if h.downcase == "e"
-      num = 15 if h.downcase == "f"
+      num = hex_lookup[h] if hex_lookup.include? h.downcase
       sum += num * (16 ** i)
     end
     sum = check_for_errors(sum)
+  end
+
+  def hex_lookup
+    {"a" => 10, "b" => 11, "c" => 12, "d" => 13, "e" => 14, "f" => 15}
   end
 
   def check_for_errors(sum)
