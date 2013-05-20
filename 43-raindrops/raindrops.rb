b1cb990dbb85fd3ec46220486c851d9776c919ab
John Maddux <jemaddux@gmail.com>
2013-05-20 09:26:43 -0600

    refactored

--- 43-raindrops/raindrops.rb
+++ 43-raindrops/raindrops.rb
@@ -4,10 +4,14 @@ class Raindrops
 
   def convert(number)
     word = ""
-    {3 => "Pling", 5 => "Plang", 7 => "Plong"}.each do |key, value|
+    primes.each do |key, value|
       word += value if number % key == 0
     end
     return word if word != ""
     number.to_s
+  end
+
+  def primes
+    {3 => "Pling", 5 => "Plang", 7 => "Plong"}
   end
 end
