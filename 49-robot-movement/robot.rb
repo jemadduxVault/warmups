63f3cd515511d74e1940d82f0c9d42905762871d
John Maddux <jemaddux@gmail.com>
2013-06-04 09:31:14 -0600

    new

--- 49-robot-movement/robot.rb
+++ 49-robot-movement/robot.rb
@@ -2,7 +2,6 @@ class Robot
   attr_accessor :orientation, :x, :y
 
   def initialize
-
   end
 
   def orient(orientation)
@@ -25,7 +24,7 @@ class Robot
   end
 
   def at(x, y)
-    @x, @y = [x, y]
+    @x, @y = x, y
   end
 
   def coordinates
@@ -36,21 +35,18 @@ class Robot
     turn(:right)
   end
 
-  def turn(way)
-    which_way = 1 if way == :right
-    which_way = -1 if way == :left
-    directions = [:north, :east, :south, :west, :north]
-    directions.each_with_index do |direction, index|
-      if @orientation == direction
-        @orientation = directions[index + which_way]
-        return @orientation
-      end
-    end
-  end
-
   def turn_left
     return @orientation = :west if @orientation == :north
     turn(:left)
   end
 
+  def turn(way)
+    directions = [:north, :east, :south, :west, :north]
+    directions.each_with_index do |direction, index|
+      if @orientation == direction
+        return @orientation = directions[index + 1] if way == :right
+        return @orientation = directions[index - 1] if way == :left
+      end
+    end
+  end
 end
