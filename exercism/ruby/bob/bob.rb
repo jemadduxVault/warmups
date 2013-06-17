b7168f4de53df1a7720016e91c4c89ed5709abc4
John Maddux <jemaddux@gmail.com>
2013-06-17 09:41:10 -0600

    adding some l33t

--- exercism/ruby/bob/bob.rb
+++ exercism/ruby/bob/bob.rb
@@ -9,4 +9,23 @@ class Bob
     return "Woah, chill out!" if input.upcase == input
     "Whatever."
   end
+
+  def l33t_sP34k(word)
+    return l33t_word(word) if l33t_word(word)
+    translation = {"e" => "3", "o" => "0", "l" => "1", "a" =>"4"}
+    translation.each do |key, value|
+      word = word.gsub(key,value)
+    end
+    word
+  end
+
+  def l33t_word(word)
+    l33t_words = {"bye" => "bai", "crap" => "carp", "cake" => "ceak",
+                  "and" => "nd", "dude" => "d00d", "from" => "form",
+                  "guys" => "guise"}
+    l33t_words.each do |key, value|
+      return value if key == word
+    end
+    false
+  end
 end
--- exercism/ruby/bob/test.rb
+++ exercism/ruby/bob/test.rb
@@ -37,4 +37,64 @@ class BobTest < MiniTest::Unit::TestCase
     assert_equal 'Fine, be that way.', bob.hey('')
   end
 
+  def test_l33t_sP34k
+    assert_equal "133t sp34k", bob.l33t_sP34k("leet speak")
+  end
+
+  def test_more_l33t_sP34k
+    assert_equal "h3110 w0r1d", bob.l33t_sP34k("hello world")
+  end
+
+  def test_lots_of_133t_words1
+    assert_equal "bai", bob.l33t_sP34k("bye")
+    assert_equal "carp", bob.l33t_sP34k("crap")
+    assert_equal "ceak", bob.l33t_sP34k("cake")
+    assert_equal "nd", bob.l33t_sP34k("and")
+    assert_equal "d00d", bob.l33t_sP34k("dude")
+    assert_equal "form", bob.l33t_sP34k("from")
+    assert_equal "guise", bob.l33t_sP34k("guys")
+    assert_equal "h4x", bob.l33t_sP34k("hacks")
+    assert_equal "4x0rz", bob.l33t_sP34k("hacks")
+  end
+
+  def test_lots_of_133t_words2
+    skip
+    assert_equal "hai", bob.l33t_sP34k("hi")
+    assert_equal "j00", bob.l33t_sP34k("you")
+    assert_equal "kewl", bob.l33t_sP34k("cool")
+    assert_equal "liek", bob.l33t_sP34k("like")
+    assert_equal "mastah", bob.l33t_sP34k("master")
+    assert_equal "max0r", bob.l33t_sP34k("max.")
+    assert_equal "max0r", bob.l33t_sP34k("maximum")
+    assert_equal "OMG", bob.l33t_sP34k("oh my god")
+  end
+
+  def test_lots_of_133t_words3
+    skip
+    assert_equal "own", bob.l33t_sP34k("own")
+    assert_equal "PMGZ", bob.l33t_sP34k("MG")
+    assert_equal "ZPMG", bob.l33t_sP34k("MG")
+    assert_equal "OLO", bob.l33t_sP34k("LOL")
+    assert_equal "ph34", bob.l33t_sP34k("fear")
+    assert_equal "pl0x", bob.l33t_sP34k("please")
+    assert_equal "powwah", bob.l33t_sP34k("power")
+  end
+
+  def test_lots_of_133t_words4
+    skip
+    assert_equal "pr0n", bob.l33t_sP34k("porn")
+    assert_equal "t3h", bob.l33t_sP34k("the")
+    assert_equal "secks", bob.l33t_sP34k("sex")
+    assert_equal "wen", bob.l33t_sP34k("when")
+  end
+
+  def test_lots_of_133t_words5
+    skip
+    assert_equal "wat", bob.l33t_sP34k("what")
+    assert_equal "winnar", bob.l33t_sP34k("winner")
+    assert_equal "u", bob.l33t_sP34k("you")
+    assert_equal "r", bob.l33t_sP34k("are")
+    assert_equal "y", bob.l33t_sP34k("why")
+  end
+
 end
