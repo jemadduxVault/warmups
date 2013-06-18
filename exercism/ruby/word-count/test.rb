0d55a18c81295c50b7c14135fcf38c4aff76e737
John Maddux <jemaddux@gmail.com>
2013-06-18 09:12:08 -0600

    done

---
+++ exercism/ruby/word-count/test.rb
@@ -0,0 +1,44 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative 'words'
+
+class WordsTest < MiniTest::Unit::TestCase
+
+  def test_count_one_word
+    words = Words.new("word")
+    counts = {"word" => 1}
+    assert_equal counts, words.count
+  end
+
+  def test_count_one_of_each
+    words = Words.new("one of each")
+    counts = {"one" => 1, "of" => 1, "each" => 1}
+    assert_equal counts, words.count
+  end
+
+  def test_count_multiple_occurrences
+    words = Words.new("one fish two fish red fish blue fish")
+    counts = {"one"=>1, "fish"=>4, "two"=>1, "red"=>1, "blue"=>1}
+    assert_equal counts, words.count
+  end
+
+  def test_ignore_punctuation
+    words = Words.new("car : carpet as java : javascript!!&@$%^&")
+    counts = {"car"=>1, "carpet"=>1, "as"=>1, "java"=>1, "javascript"=>1}
+    assert_equal counts, words.count
+  end
+
+  def test_include_numbers
+    words = Words.new("testing, 1, 2 testing")
+    counts = {"testing" => 2, "1" => 1, "2" => 1}
+    assert_equal counts, words.count
+  end
+
+  def test_normalize_case
+    words = Words.new("go Go GO")
+    counts = {"go" => 3}
+    assert_equal counts, words.count
+  end
+
+end
+
