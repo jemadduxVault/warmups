183b1fdb4a2fe682b0e88cf35e099adafb9195ec
John Maddux <jemaddux@gmail.com>
2013-06-19 09:07:19 -0600

    done

--- exercism/ruby/word-count/words.rb
+++ exercism/ruby/word-count/words.rb
@@ -3,16 +3,14 @@ class Words
 
   def initialize(words)
     @words = words.downcase.gsub(/[^a-z0-9 ]/,"")
+    # @words = words.downcase.gsub(/[\W]/,"") ##Can't use this one because it also gets the blank spaces. I could move it
   end
 
   def count
     answer = {}
     words.split(" ").each do |word|
-      if answer[word]
-        answer[word] += 1
-      else
-        answer[word] = 1
-      end
+      answer[word] += 1 if answer[word]
+      answer[word] = 1 unless answer[word]
     end
     answer
   end
