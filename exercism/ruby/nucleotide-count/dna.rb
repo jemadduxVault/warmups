cfe674ce3d0bacc0855f4e0e187ae642a5958dee
John Maddux <jemaddux@gmail.com>
2013-07-09 08:58:34 -0600

    Another assignment

--- exercism/ruby/nucleotide-count/dna.rb
+++ exercism/ruby/nucleotide-count/dna.rb
@@ -1,20 +1,22 @@
 class DNA
-  attr_reader :nucleotide
+  attr_reader :nucleotides
 
-  def initialize(nucleotide)
-    @nucleotide = nucleotide.split("")
+  Nucleotides_Whitelist = ["B", "D", "E", "F", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "V", "W", "Z"]
+
+  def initialize(nucleotides)
+    @nucleotides = nucleotides.split("")
   end
 
-  def count(gcat)
-    raise ArgumentError if gcat == "X"
-    return 0 unless ["G","C","A","T"].include? gcat
-    nucleotide_counts[gcat]
+  def count(nucleotide)
+    raise ArgumentError unless Nucleotides_Whitelist.include? nucleotide
+    return 0 unless ["G","C","A","T"].include? nucleotide
+    nucleotide_counts[nucleotide]
   end
 
   def nucleotide_counts
     answer = {"A"=>0, "T"=>0, "C"=>0, "G"=>0}
-    nucleotide.each do |tide|
-      answer[tide] += 1
+    nucleotides.each do |nucleotide|
+      answer[nucleotide] += 1
     end
     answer
   end
